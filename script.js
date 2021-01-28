0718f05def9323c572bdbc2d8ab271b4beb5ea4f
TATSIANA <tannysha1987@mail.ru>
2021-01-28 09:23:59 +0300

    feat:add main

---
+++ script.js
@@ -0,0 +1,18 @@
+let offset = 0;
+const slider = document.querySelector('.slider_line');
+
+document.querySelector('.slider_next').addEventListener('click', function() {
+    offset = offset += 300;
+    if (offset > 1020) {
+            offset = 0;
+    }
+  sliderLine.style.left = -offset + 'px';
+})
+
+document.querySelector('.slider_prev').addEventListener('click',function() {
+    offset = offset -= 300;
+    if (offset < 1020) {
+            offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+})
